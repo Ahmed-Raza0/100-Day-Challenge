926c1fcf6342c9c2f1736d9717a3a088ca6f8329
Ahmed-Raza0 <Ahed80174@gmail.com>
2024-06-25 22:23:05 +0500

    first day

---
+++ 1-day/main.ts
@@ -0,0 +1,20 @@
+// Question 1:
+
+// install node js 
+
+// Question 2: Personal Message: Store a person’s name in a variable
+//  and print a message to them, like “Hello Asharib, would you like to
+//   learn some TypeScript today?”
+
+let myName: string = "Ahmed";
+console.log(`Hello ${myName}, would you like to learn some TypeScript today?`);
+
+
+// Question 3:  Name Cases: Store a person’s name in a variable,
+// and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let nam: string = "Ahmed";  
+
+console.log(nam.toLowerCase()); 
+console.log(nam.toUpperCase());  
+console.log(nam.charAt(0).toUpperCase() + nam.slice(1).toLowerCase());  
