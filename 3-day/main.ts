106f8f82e0df108c0ad4c06d062f2f60890b79a8
Ahmed-Raza0 <Ahed80174@gmail.com>
2024-06-25 22:47:48 +0500

    third day

---
+++ 3-day/main.ts
@@ -0,0 +1,26 @@
+// Question 1: Number Eight: Write addition, subtraction, multiplication,
+//  and division operations that each result in the number 8. Enclose your 
+//  operations in print statements.
+
+console.log(5 + 3); 
+console.log(10 - 2); 
+console.log(4 * 2); 
+console.log(16 / 2);  
+
+
+// Question 2: You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line
+
+
+// Answer 2:same the question 1
+
+
+// Question 3: Favorite Number: Store your favorite number in a variable. 
+//  Then, using that variable, create a message that reveals your favorite number. 
+//  Print that message.
+
+
+// Answer 3:
+const favoriteNumber: number = 8;
+console.log(`My favorite number is ${favoriteNumber}.`);
