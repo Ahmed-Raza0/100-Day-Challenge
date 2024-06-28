2f914aa41b80a6f457a44eb09abf4cf55d04cf62
Ahmed-Raza0 <Ahed80174@gmail.com>
2024-06-28 22:30:26 +0500

    nine day

---
+++ 9-day/main.ts
@@ -0,0 +1,33 @@
+// Question 25: Alien Colors #1: Imagine an alien was just shot down in a game.
+//  Assign 'green', 'yellow', or 'red' to a variable called alien_color.
+
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("You just earned 5 points!");
+}
+
+// No output because the condition is false
+
+
+// Question 26: Alien Colors #2: Choose a color for an alien, then write an if-else chain.
+
+alien_color = "green";
+if (alien_color == "green") {
+    console.log("You just earned 5 points for shooting the alien!");
+} else {
+    console.log("You just earned 10 points.");
+}
+
+
+// Question 27: Alien Colors #3: Convert your if-else chain to handle three 
+// colors: green, yellow, red.
+
+
+alien_color = "green";
+if (alien_color == "green") {
+    console.log("You earned 5 points.");
+} else if (alien_color == "yellow") {
+    console.log("You earned 10 points.");
+} else if (alien_color == "red") {
+    console.log("You earned 15 points.");
+}
